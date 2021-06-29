d25a56fbf8663d1309532b93950c5949288a48b8
scarletcamomile <scarletcamomile@mail.ru>
2021-06-29 13:41:46 +0300

    fix styles and add link to business

--- src/components/pages/Businesses.jsx
+++ src/components/pages/Businesses.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
 import { DataGrid } from '@material-ui/data-grid';
 import { GET_ALL_BUSINESSES } from '../../gql/queries/getAllBusinesses';
-
 import { Loading } from '../common/Loading';
-import Container from '@material-ui/core/Container';
+
 const Businesses = () => {
   /*
     commented code is to avoid querying all businesses in Leanid's local
@@ -33,47 +34,57 @@ const Businesses = () => {
   }
 
   const columns = [
-    { field: 'name', headerName: 'Business Name', width: 200, sortable: false },
+    {
+      field: 'name',
+      headerName: 'Business Name',
+      width: 200,
+      sortable: false,
+      flex: 1.5,
+      disableClickEventBubbling: true,
+      renderCell: (params) => (
+        <Link to={`/businesses/${params.id}`}>{params.value}</Link>
+      ),
+    },
     {
       field: 'industry',
       headerName: 'Industry',
-      width: 200,
+      flex: 1,
       sortable: false,
     },
     {
       field: 'subindustry',
       headerName: 'Sub-Industry',
-      width: 200,
+      flex: 1,
       sortable: false,
     },
     {
       field: 'contactEmail',
       headerName: 'Contact Email',
-      width: 110,
+      flex: 1.5,
       sortable: false,
     },
     {
       field: 'contactPhone',
       headerName: 'Сontact Phone',
-      width: 110,
+      flex: 0.5,
       sortable: false,
     },
     {
       field: 'createdAt',
       headerName: 'Created At',
-      width: 110,
+      flex: 0.5,
       sortable: false,
     },
     {
       field: 'updatedAt',
       headerName: 'Updated At',
-      width: 110,
+      flex: 0.5,
       sortable: false,
     },
     {
       field: 'uuid',
       headerName: 'UUID',
-      width: 200,
+      flex: 1.5,
       sortable: false,
     },
   ];
@@ -81,12 +92,12 @@ const Businesses = () => {
   const rows = data?.businesses?.edges?.map((business) => business.node);
 
   return (
-    <Container>
+    <Container maxWidth={false}>
       <h1>Businesses</h1>
       {loading ? (
         <Loading />
       ) : (
-        <div style={{ width: '100%', height: 968 }}>
+        <div style={{ width: '100%', height: '89vh' }}>
           <DataGrid rows={rows} columns={columns} pageSize={20} />
         </div>
       )}
