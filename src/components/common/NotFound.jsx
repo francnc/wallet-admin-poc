e422715ddabfe900ec5cb69d0098170b5c7686b8
sprnz <39864352+eamayag@users.noreply.github.com>
2021-06-30 09:16:25 +0200

    Homepage & NotFoundPage (#9)
    * add homepage
    * add NotFound styles
    * update styles for notFound icon
    * refactor tab information

--- src/components/common/NotFound.jsx
+++ src/components/common/NotFound.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+
+const errorStyles = {
+  margin: '0 auto',
+  marginTop: '5%',
+  padding: '30px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  height: 'fit-content',
+  minHeight: '400px',
+  minWidth: '800px',
+};
+
+const useStyles = makeStyles({
+  root: {
+    border: 0,
+    color: 'red',
+    height: 120,
+    padding: '0 30px',
+    width: '5em',
+  },
+});
 
 const NotFound = () => {
-  return <p>Not Found!</p>;
+  const classes = useStyles();
+
+  return (
+    <div style={errorStyles}>
+      <ErrorOutlineIcon
+        classes={{ root: classes.root }}
+        fontSize="large"
+        centered
+      />
+      <h1>Oops! Page not found (404)!</h1>
+    </div>
+  );
 };
 
 export { NotFound };
---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePage = () => {
+  return <h1 style={{ margin: '20px' }}>Welcome to Wallet-Admin</h1>;
+};
+
+export { HomePage };
